64b26b99389c3753a6ce70c6a4e379b89b52f2b8
luigilalo <luigilaloe1@gmail.com>
2023-12-20 15:52:12 +0100

    done without bonus

--- db-university-query.sql
+++ db-university-query.sql
@@ -11,9 +11,29 @@ select *
 from students s 
 where year(s.date_of_birth)= 1990;
 
--- 4)
+-- 4) Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
+select *
+from courses c
+where c.period = 'I semestre' and  c.`year` =1;
 
 -- 5) Da quanti dipartimenti è composta l’università?
 select count(*)
 from departments d ;
 
+-- 6) Contare tutti gli insegnanti il cui cognome termina con la “E” (8)
+select count(*)
+from teachers t 
+where t.surname like '%E';
+
+-- 7) Selezionare nome, cognome e codice fiscale di tutti gli studenti iscritti al Corso di Laurea in Economia, in ordine alfabetico
+select s.name , s.surname , s.fiscal_code
+from students s
+join `degrees` d on s.degree_id = d.id 
+where d.name = 'Corso di Laurea in Economia'
+order by s.surname , s.name , s.fiscal_code;
+
+-- 8) Selezionare tutti i Corsi di Laurea Magistrale del Dipartimento di Neuroscienze
+SELECT *
+FROM degrees d 
+JOIN departments d2 ON d.department_id = d2.id 
+WHERE d.`level` = 'magistrale' AND d2.name = 'Dipartimento di Neuroscienze';
